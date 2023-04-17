ed69825432022d82e431909ec3f47b5368f421a4
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-17 15:14:43 +0200

    routes added to prevent user post method input with get routes

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,30 @@ use Illuminate\Http\Request;
 */
 
 Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/profile',function(){
+        return view('profile');
+    })->name('profile');
     Route::get('/register', 'register')->name('register');
+    Route::get('/store',function(){
+        return redirect()->route("homepage");
+    });
     Route::post('/store', 'store')->name('store');
     Route::get('/login', 'login')->name('login');
+    Route::get('/authenticate', function(){
+        return redirect()->route("homepage");
+    })->name('authenticate');
     Route::post('/authenticate', 'authenticate')->name('authenticate');
     Route::get('/', 'homepage')->name('homepage');
-    Route::get('/logout', 'logout')->name('logout');;
+    Route::get('/logout', 'logout')->name('logout');
     Route::controller(PhotoController::class)->group(function(){
-        Route::get('/photos/create','create')->name('photos.create');
         Route::delete('/photos/{filename}','destroy')->name('photos.destroy');
+        Route::get('/photos/download/{filename}', function (){
+            return redirect()->route('homepage');
+        });
         Route::post('/photos/download/{filename}','download')->name('photos.download');
-        Route::post('/photos','store')->name('photos.store');
+        Route::get('/photos/upload',function(){
+            return redirect()->route('homepage');
+        });
         Route::post('/photos/upload','upload')->name('photos.upload');
     });
 });
