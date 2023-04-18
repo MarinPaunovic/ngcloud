f9765f6e15102b25f9f4bfac5ac4ba841da087a0
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-18 15:36:04 +0200

    skelet for profile added

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
+    <?php
+    use Illuminate\Support\Facades\App;
+    
+    $environment = App::environment();
+    dd(App::environment('local'));
+    ?>
     <div>Profile</div>
+
+    <form>
+
+
+    </form>
 @endsection
