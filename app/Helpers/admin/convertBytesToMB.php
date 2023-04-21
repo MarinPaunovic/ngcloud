d71f37e781a1c29257ea96bce497609be6930cd9
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-21 11:49:00 +0200

    change file path

--- app/Helpers/admin/convertBytesToMB.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (! function_exists('convertBytesToMB')) {
-    function convertBytesToMB($size) {
-        $sizeMB=number_format($size / 1024 / 1024,2);
-        return (float)$sizeMB;
-    }
-}
