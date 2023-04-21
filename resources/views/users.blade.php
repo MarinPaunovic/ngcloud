020f9d946389937620ff93982176d5bee31b90eb
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-21 10:21:58 +0200

    add elements to show each user role and storage taken for same user

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,8 +3,12 @@
 @section('content')
     @foreach ($users as $user)
         <div class="grid">
-            <div>{{ $user }}</div>
-            <div>{{ }}</div>
+            <div>{{ $user->name }}</div>
+            @foreach ($sizes as $size)
+                @if (key($size) == $user->id)
+                    <div>{{ $size[$user->id] }}</div>
+                @endif
+            @endforeach
         </div>
     @endforeach
 @endsection
