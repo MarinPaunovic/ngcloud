56de03b9f2cfeb3f6abeb9ea0e90de3bb7766a3d
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-21 15:09:45 +0200

    create blade for mail view

---
+++ resources/views/emails/subscribers.blade.php
@@ -0,0 +1,10 @@
+<x-mail::message>
+    <div>
+        <p>Reset your password
+            <a href="{{ $link }}">
+                here
+            </a>
+        </p>
+    </div>
+
+</x-mail::message>
