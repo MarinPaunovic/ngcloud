5f0361caf4184668d28047f83b81aa8ff8573bc4
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-21 15:06:28 +0200

    turn empty test controller to pw reset controller(mail controller)

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Test;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Config;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\TestMail;
+use App\Models\Test;
+use App\Mail\VerifyEmail;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class TestController extends Controller
 {
@@ -15,73 +28,37 @@ class TestController extends Controller
      */
     public function index(Request $request)
     {
-        $user = $request->user();
-        return $user;
+        return view('test');
+         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function reset(Request $request){
+
+        $tokenExists=DB::table('password_resets')->where("email",$request->email)->first();
+        if($tokenExists == null){
+            DB::table('password_resets')->insert([
+                'email' => $request->email,
+                'token' => rand(100000,1000000),
+                'created_at' => Carbon::now()
+            ]);}
+            
+            $tokenData = DB::table('password_resets')
+            ->where('email', $request->email)->first();
+        if ($this->sendResetEmail($request->email, $tokenData->token)) {
+            return redirect()->back()->with('status', trans('A reset link has been sent to your email address.'));
+        } else {
+            return redirect()->back()->withErrors(['error' => trans('A Network Error occurred. Please try again.')]);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Test  $test
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Test $test)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Test  $test
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Test $test)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Test  $test
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Test $test)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Test  $test
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Test $test)
-    {
-        //
-    }
+    private function sendResetEmail($email, $token)
+{
+$user = DB::table('users')->where('email', $email)->select('name', 'email')->first();
+$link = Config::get('app')['url'] . '/password/reset/' . $token . '?email=' . urlencode($user->email);
+if($link){
+    Mail::to($email)->send(new TestMail($link));
+    return true;
+}
+ return false;
+}
 }
