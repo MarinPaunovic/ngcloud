b2adb8fdb6c3ad8402043fff6338bc99fdd0ac16
MarinPaunovic <marinpaunovic@gmail.com>
2023-04-25 09:15:48 +0200

    change markdown to password from subscribers

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -43,7 +43,7 @@ class TestMail extends Mailable
     public function content()
     {
         return new Content(
-            markdown: 'emails.subscribers',
+            markdown: 'emails.password',
             with: ['link'=> $this->link]
         );
     }
